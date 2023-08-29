b26a5532ee839b47b7fba2d5843d81f001a3f61c
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-29 18:05:36 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">
+    IA Modpanel Demo
+</h1>
+
+> **Note**
+> The `modpanel-game` doesn't contain any scripts yet
+
+This is a demo to showcase how a theoretical modpanel for Item Asylum could look like, the `base-game` and `modpanel-game` folders contain scripts that should be synced to Roblox using [Rojo](https://rojo.space)  
+The two games communicate using a DataStore and MessagingService
+
+# MessagingService Schema
+
+## `modpanel` topic
+
+Arguments:
+
+- `id` — string, represents the unique ID of the message (conversation), used for acking
+- `serverID` — string, the target server ID
+- `action` — the modpanel action
+- `args` — array, an array of arguments for the specific action
+
+### `shutdown` action
+
+No args needed
+
+### `kickPlr` action
+
+- `plr` — string, the player's username
+- `reason` — string, why the player got kicked
+
+## `modpanel-ack`
+
+Arguments:
+
+- `id` — string, the unique ID of a message (conversation)
---
+++ base-game/src/server/ModPanel.server.lua
@@ -0,0 +1,132 @@
+-- types --
+type PlayerData = {
+	id: number,
+	name: string,
+	joinedPlayer: number?,
+	joinedAt: number,
+	leftAt: number?,
+}
+type ServerData = {
+	players: { PlayerData },
+	round: {
+		playing: boolean,
+		map: string,
+		mode: string,
+		startedPlayingAt: boolean,
+	},
+}
+
+-- top level stuff --
+local DataStoreService = game:GetService("DataStoreService")
+local MessagingService = game:GetService("MessagingService")
+local Players = game:GetService("Players")
+
+local DB = DataStoreService:GetDataStore("modpanel")
+local serverID = game.JobId
+
+-- ignore private servers
+if game.PrivateServerOwnerId ~= 0 then
+	return
+end
+
+local players: { [Player]: PlayerData } = {}
+
+-- important --
+local function grabData(): ServerData
+	local data: ServerData = {
+		players = {},
+	}
+	for _, x in pairs(players) do
+		data.players[#data.players + 1] = x
+	end
+
+	return data
+end
+
+local queueID
+local function queueUpdate()
+	local now = tick()
+	queueID = now
+
+	task.delay(10, function()
+		if queueID == now then
+			DB:SetAsync(serverID, grabData())
+		end
+	end)
+end
+
+local function ackModpanelMessage(id: string, success: boolean)
+	MessagingService:PublishAsync("modpanel-ack", { id, success })
+end
+
+-- runners --
+game:BindToClose(function()
+	DB:UpdateAsync("list", function(list)
+		list = list or {}
+		if table.find(list, serverID) then
+			table.remove(list, table.find(list, serverID))
+		end
+
+		return list
+	end)
+	queueID = nil
+	DB:SetAsync(serverID, nil)
+end)
+
+game.Players.PlayerAdded:Connect(function(plr)
+	players[plr] = {
+		id = plr.UserId,
+		name = plr.Name,
+		joinedPlayer = plr.FollowUserId,
+		joinedAt = DateTime.now().UnixTimestampMillis,
+	}
+	queueUpdate()
+end)
+game.Players.PlayerRemoving:Connect(function(plr)
+	players[plr] = players[plr]
+		or {
+			id = plr.UserId,
+			name = plr.Name,
+			joinedPlayer = plr.FollowUserId,
+			joinedAt = 0,
+		}
+
+	players[plr].leftAt = DateTime.now().UnixTimestampMillis
+	queueUpdate()
+
+	task.wait(60)
+
+	players[plr] = nil
+	queueUpdate()
+end)
+
+MessagingService:SubscribeAsync("modpanel", function(msg)
+	local data = msg.Data
+
+	if data[2] ~= serverID then
+		return
+	end
+
+	if data[3] == "shutdown" then
+		for _, x in pairs(Players:GetPlayers()) do
+			x:Kick("Shutting down server...")
+		end
+		ackModpanelMessage(data[1], true)
+	elseif data[3] == "kickPlr" then
+		local plr: Player? = Players:FindFirstChild(data[4][1])
+		if not plr then
+			return ackModpanelMessage(data[1], false)
+		end
+
+		plr:Kick(data[4][1])
+		ackModpanelMessage(data[1], true)
+	end
+end)
+
+-- init --
+DB:UpdateAsync("list", function(list)
+	list = list or {}
+	list[#list + 1] = serverID
+
+	return list
+end)
